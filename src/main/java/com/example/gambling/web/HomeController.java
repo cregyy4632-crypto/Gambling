46106d49c30fc441ce9090b2e9e0ed32bf49c903
ivan <cregyy4632@gmail.com>
2025-09-06 15:04:37 +0100

    Create Epic Gambling By IC website with home page and navigation

--- src/main/java/com/example/gambling/web/HomeController.java
+++ src/main/java/com/example/gambling/web/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String index() {
-        return "blackjack";
+        return "index";
     }
     
     @GetMapping("/blackjack")
